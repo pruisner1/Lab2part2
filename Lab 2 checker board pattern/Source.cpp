058a70d6aeab38cb4b4925898f77b39c5a5939d5
pruisner1 <54373319+pruisner1@users.noreply.github.com>
2019-09-04 13:26:01 -0500

    Add project files.

---
+++ Lab 2 checker board pattern/Source.cpp
@@ -0,0 +1,32 @@
+/* 
+Lane Pruisner
+C++ Fall 2019
+Due September 12, 2019
+Lab #2 Checker Board Pattern
+Create a evenly spaced checker board pattern
+*/
+#include <iostream>
+
+using namespace std;
+// Variables "S" and "A" represent which set of spacing and astrics are used //
+const string S = "* * * *";
+const string A = " * * * *";
+// cout to output the variables //
+int main()
+{
+	cout << S << endl;
+	cout << A << endl;
+	cout << S << endl;
+	cout << A << endl;
+	cout << S << endl;
+	cout << A << endl;
+	cout << S << endl;
+	cout << A << endl;
+
+	int var;
+	cin >> var;
+
+
+
+		return (0);
+}
